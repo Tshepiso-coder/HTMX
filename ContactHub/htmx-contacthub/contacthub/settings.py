dde33a44efc2ee1a70e8b831ef68a9c4cce777d6
Tshepiso <lesegograce5@gmail.com>
2025-12-08 16:50:57 +0200

    update settings

--- ContactHub/htmx-contacthub/contacthub/settings.py
+++ ContactHub/htmx-contacthub/contacthub/settings.py
@@ -131,6 +131,7 @@ STORAGES = {
      }
 }
 
+# AWS storage keys
 AWS_S3_ACCESS_KEY_ID = os.environ.get('AWS_S3_ACCESS_KEY_ID')
 AWS_S3_SECRET_ACCESS_KEY = os.environ.get('AWS_S3_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME ='django-container'
